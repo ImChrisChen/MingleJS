0169a3f3e4c63027aa2779ef0dc127af8e70e3a5
chrischen <309129685@qq.com>
2020-09-21 00:33:58 +0800

    feat: 组件渲染结构调整，保证每一个组件都用独立的容器

--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
-import 'antd/dist/antd.css';import './src/App.scss';import App from './src/App';window.onload = function () {    // 类数组转成数组 NodeListOf<HTMLElement>  => Array<HTMLElement>    let elements = Array.from(document.querySelectorAll(`[data-fn]`));    new App(elements);};
+import 'antd/dist/antd.css';
+import './src/App.scss';
+import App from './src/App';
+
+window.onload = function () {
+    // 类数组转成数组 NodeListOf<HTMLElement>  => Array<HTMLElement>
+    let elements:Array<HTMLElement> = Array.from(document.querySelectorAll(`[data-fn]`));
+    new App(elements);
+};
