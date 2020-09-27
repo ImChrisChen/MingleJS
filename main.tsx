189c4bd719a87d8b14bd803aa8abaf0d998b8c8a
ChrisChen <309129685@qq.com>
2020-09-27 18:57:00 +0800

    feat: 深色模式

--- main.tsx
+++ main.tsx
@@ -1,10 +1,23 @@
-// import 'antd/dist/antd.compact.less'    // 紧凑模式
-import 'antd/dist/antd.min.css'
+import 'antd/dist/antd.compact.less' // 紧凑模de式
 
 import './src/App.scss';
 import React from 'react';
 import App from './src/App';
 import { message } from "antd";
+// import 'antd/dist/antd.dark.css'
+
+// 判断是否是深色模式
+const darkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)');
+// 判断是否匹配深色模式
+if (darkMode && darkMode.matches) {
+
+}
+
+// 监听主题切换事件
+darkMode && darkMode.addEventListener('change', e => {
+    // e.matches true 深色模式
+});
+
 
 let env = process.env.NODE_ENV;
 console.log(env);
@@ -20,7 +33,6 @@ console.log(env);
 //     // 类数组转成数组 NodeListOf<HTMLElement>  => Array<HTMLElement>
 //     // };
 // }
-
 
 
 window.onload = function () {
