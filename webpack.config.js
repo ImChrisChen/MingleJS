81385b6307537304630e38b2e35958d5f9b7c5a9
chenjian <chenjian@aidalan.com>
2021-03-19 13:46:41 +0800

    feat: webpack打包处理

--- webpack.config.js
+++ webpack.config.js
@@ -47,13 +47,15 @@ module.exports = {
     devtool: isProduction ? 'cheap-module-source-map' : 'cheap-module-source-map',     // https://www.cnblogs.com/cl1998/p/13210389.html
     entry: {            // 分文件打包
         // [name]是对应的入口文件的key, [name].js 就是main.js
-        main: './main.tsx',    // https://webpack.js.org/guides/code-splitting/
+        main: isProduction
+            ? './main.production.ts'
+            : './main.tsx',    // https://webpack.js.org/guides/code-splitting/
         // vendoer: [
         //     'react',
         //     'react-dom',
         //     'antd',
         // ],
-        chart: ['bizcharts'],
+        // chart: ['bizcharts'],
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
