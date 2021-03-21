bb9d4033d80acdeee92b29189de3ccd730193774
ChrisChen <309129685@qq.com>
2021-03-21 13:03:10 +0800

    feat: 成功从webpack-v4 升级到v5，解决构建等问题

--- main.prod.ts
+++ main.prod.ts
@@ -1,7 +1,7 @@
 import './src/App.scss';
 import 'antd/dist/antd.compact.less'; // 紧凑模de式
 import { Mingle } from './src/core/Mingle';
-import { message, notification } from '_antd@4.14.0@antd';
+import { message, notification } from 'antd';
 
 window['Mingle'] = Mingle;
 window['Message'] = message;
