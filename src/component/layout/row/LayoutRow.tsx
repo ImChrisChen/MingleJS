d89c97515ea22b1bea2257eba1b87dd880c833e7
chenjian <chenjian@aidalan.com>
2021-03-30 10:44:39 +0800

    fixed: LayoutRow.tsx LayoutCol.tsx  cssText 重复

--- src/component/layout/row/LayoutRow.tsx
+++ src/component/layout/row/LayoutRow.tsx
@@ -6,14 +6,30 @@
  */
 
 import { INativeProps } from '@interface/common/component';
+import { isObject } from '@utils/inspect';
 
 export default class LayoutRow {
 
     constructor(props: INativeProps) {
         let el = props.el;
-        el.style.display = 'flex';
-        el.style.minHeight = '90px';
-        el.style.cssText = String(props.cssText);
+        let style = {
+            display  : 'flex',
+            minHeight: '90px',
+        };
+        el.style.cssText = this.mergeStyle(style, props.cssText);
+    }
+
+    mergeStyle(style: object, cssText: string = '') {
+        let text = '';
+        if (isObject(style)) {
+            for (const k in style) {
+                if (!style.hasOwnProperty(k)) continue;
+                let v = style[k];
+                text += `${ k }:${ v };`;
+            }
+        }
+        text = text + cssText;
+        return text;
     }
 
 }
--- src/component/layout/row/col/LayoutCol.tsx
+++ src/component/layout/row/col/LayoutCol.tsx
@@ -6,16 +6,35 @@
  */
 
 import { INativeProps } from '@interface/common/component';
+import { isObject } from '@utils/inspect';
 
 export default class LayoutCol {
 
     constructor(props: INativeProps) {
         let { col } = props.dataset;
         let { el } = props;
-        el.style.width = (col / 24 * 100) + '%';
-        el.style.minHeight = '90px';
-        el.style.margin = '0 4px';
-        el.style.border = '1px solid #f0f';
-        el.style.cssText = String(props.cssText);
+
+        // TODO 直接设置 cssText 会把行内样式覆盖掉
+        let style = {
+            width    : (col / 24 * 100) + '%',
+            margin   : '0 4px',
+            minHeight: '90px',
+            border   : '1px solid #f0f',
+        };
+
+        el.style.cssText = this.mergeStyle(style, props.cssText);
+    }
+
+    mergeStyle(style: object, cssText: string = '') {
+        let text = '';
+        if (isObject(style)) {
+            for (const k in style) {
+                if (!style.hasOwnProperty(k)) continue;
+                let v = style[k];
+                text += `${ k }:${ v };`;
+            }
+        }
+        text = text + cssText;
+        return text;
     }
 }
